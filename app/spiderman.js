2589b8401bab3468b65976c9eb0921c0d6fc32ee
D-CIoud <36202151+D-CIoud@users.noreply.github.com>
2022-04-24 18:11:00 -0500

    Write a test for the next bit of functionality you want to add

---
+++ app/spiderman.js
@@ -0,0 +1,12 @@
+class spiderman {
+    constructor(name,age,actor,nummovies,production){
+        this.name = name;
+        this.age = age;
+        this.actor = actor;
+        this.nummovies = nummovies;
+        this.production = production;
+    }
+}
+
+//Exportamos la clase
+module.exports = spiderman
--- test/spiderman.test.js
+++ test/spiderman.test.js
@@ -1,6 +1,13 @@
-describe("Test Suite Dummy Description", () => {
-  test("Case 1 Dummy", () => {
-    const resultOfSomething = 1 + 2;
-    expect(resultOfSomething).toBe(3);
+//const spiderman = require("../app/spiderman");
+
+describe("Unit Tests for Spider-Man Class", () => {
+  test("1) Create an Spider-Man object", () => {
+    //Instanciamos un objeto de la clase Spider-Man y lo cargamos con datos
+    const andrewGarfield = new spiderman("Spider-Man Sony",31,"Andrew Garfield",2,"Sony Pictures")
+    expect(andrewGarfield.name).toBe("Spider-Man Sony")
+    expect(andrewGarfield.age).toBe(31)
+    expect(andrewGarfield.actor).toBe("Andrew Garfield")
+    expect(andrewGarfield.nummovies).toBe(2)
+    expect(andrewGarfield.production).toBe("Sony Pictures")
   });
 });
